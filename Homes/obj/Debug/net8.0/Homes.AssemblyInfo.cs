a823a00547e37614243642bf438e179a82467339
IsmaelSabri <isma_230cv@hotmail.com>
2024-06-21 01:37:57 +0200

    hide extra files

--- Homes/obj/Debug/net8.0/Homes.AssemblyInfo.cs
+++ Homes/obj/Debug/net8.0/Homes.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71608416996d78d0960728b2d51aba05791faa53")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4bfcb1c118d99badce636647f987569abb5aeb8")]
->>>>>>> 094059416b7bff5ced01bbc0fd54b4f64f11b047
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a13e42d685cac69c275c36907622e94c53ad0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
